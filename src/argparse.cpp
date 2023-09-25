33dff293f1402cb65117d47e9ecccb198c8f1081
Blue Lizard <jlpeterson@outlook.com>
2023-09-25 01:46:30 +0000

    add argparse

---
+++ src/argparse.cpp
@@ -0,0 +1,87 @@
+#include <argparse.h> 
+
+void get_opts(int argc,
+              char **argv,
+              struct options_t *opts)
+{
+    if (argc == 1)
+    {
+        std::cout << "Usage:" << std::endl;
+        std::cout << "\t[Optional] -k <num_cluster>" <<std::endl;
+        std::cout << "\t -d <dims>" << std::endl;
+        std::cout << "\t-i <file_path>" << std::endl;
+        std::cout << "\t[Optional] -m <max_num_iters>" << std::endl;
+        std::cout << "\t[Optional] -t <threshold_convergence>" << std::endl;
+        std::cout << "\t[Optional] -c" << std::endl;
+        std::cout << "\t-s <rand_seed>" << std::endl;
+        std::cout << "\t-v <version>" << std::endl;
+        exit(0);
+    }
+
+    opts->num_cluster = 1;
+    opts->centroids = false;
+    opts->max_num_iter = 1000;
+    opts->threshold = 0.02;
+    opts->centroids = false;
+    opts->seed = 69;
+    
+    struct option l_opts[] = {
+        {"num_cluster", no_argument, NULL, 'k'},
+        {"dims", required_argument, NULL, 'd'},
+        {"input", required_argument, NULL, 'i'},
+        {"max_num_iter", no_argument, NULL, 'm'},
+        {"threshold", no_argument, NULL, 't'},
+        {"centroids", no_argument, NULL, 'c'},
+        {"seed", no_argument, NULL, 's'},
+        {"version", required_argument, NULL, 'v'},
+    };
+    
+    int ind, c;
+    while ((c = getopt_long(argc, argv, "i:o:n:l:s", l_opts, &ind)) != -1)
+    {
+        switch (c)
+        {
+        case 0:
+            break;
+        case 'k':
+            opts->num_cluster = atoi((char *)optarg);
+            break;
+        case 'd':
+            opts->dims = atoi((char *)optarg);
+            break;
+        case 'i':
+            opts->in_file = (char *)optarg;
+            break;
+        case 'm':
+            opts->max_num_iter = atoi((char *)optarg);
+            break;
+        case 't':
+            opts->threshold = atof((char *)optarg);
+            break;
+        case 'c':
+            opts->centroids = true;
+            break;
+        case 's':
+            opts->seed = atoi((char *)optarg);
+            break;
+        case 'v':
+            char* inp = (char *)optarg;
+            if (strcmp(inp,"seq") == 0 || strcmp(inp,"sequential") == 0) {
+                opts->version = sequential;
+            }
+            if (strcmp(inp,"cuda") == 0 || strcmp(inp,"cuda_basic") == 0) {
+                opts->version = cuda_basic;
+            }
+            if (strcmp(inp,"shmem") == 0 || strcmp(inp,"cuda_shmem") == 0) {
+                opts->version = cuda_shmem;
+            }
+            if(strcmp(inp,"thrust") == 0 || strcmp(inp,"cuda_thrust") == 0) {
+                opts->version = cuda_thrust;
+            }
+            break;
+        case ':':
+            std::cerr << argv[0] << ": option -" << (char)optopt << "requires an argument." << std::endl;
+            exit(1);
+        }
+    }
+}
---
+++ src/argparse.h
@@ -0,0 +1,26 @@
+#ifndef _ARGPARSE_H
+#define _ARGPARSE_H
+
+#include <getopt.h>
+#include <stdlib.h>
+#include <iostream>
+#include <string.h>
+
+const int sequential = 0;
+const int cuda_basic = 1;
+const int cuda_shmem = 2;
+const int cuda_thrust = 3;
+
+struct options_t {
+    int num_cluster;
+    int dims;
+    char *in_file;
+    int max_num_iter;
+    double threshold;
+    bool centroids;
+    int seed;
+    int version;
+};
+
+void get_opts(int argc, char **argv, struct options_t *opts);
+#endif
