28d77b335aa0645bfe3bf00ab9ca5ff27b023b6a
John L. Peterson <jlpeterson@utexas.edu>
2023-10-15 17:06:07 -0400

    working hash-worker parallelization

---
+++ run_tests-q2.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+import os
+from subprocess import check_output
+import re
+from time import sleep
+import matplotlib
+
+matplotlib.use("Agg")
+from matplotlib import pyplot as plt
+
+#
+#  Feel free (a.k.a. you have to) to modify this to instrument your code
+#
+
+NUM_SAMPLES = 10
+THREADS = [0, 8, 16]
+LOOPS = range(1, 801, 8)
+inps = ["simple.txt", "coarse.txt", "fine.txt"]
+lengths = [12, 12, 12]
+iteration_max = 12
+csvs = []
+for idx, inp in enumerate(inps):
+    times = {}
+    print(f"starting processing for file {inp}")
+    if iteration_max > lengths[idx]:
+        step = 1
+    else:
+        step = lengths[idx] // iteration_max
+    hash_workers = range(1, lengths[idx] + 1, step)
+
+    for hw in hash_workers:
+        cmd = f"./bin/BST -filename=input/{inp} -hash-workers={hw}"
+        for i in range(NUM_SAMPLES):
+            out = check_output(cmd, shell=True).decode("ascii")
+            # print(f"output: {out}")
+            m = re.search(r"[-+]?\d*\.\d+e[-+]?\d+|\b\d+\.\d+\b", out)
+            # print(f"m: {m}")
+            if m:
+                time = float(m.group())
+                if hw not in times:
+                    times[hw] = [time]
+                else:
+                    times[hw].append(time)
+    avg_times = []
+    for hw in hash_workers:
+        avg_times.append(sum(times[hw]) / len(times[hw]))
+    for i, avg_time in enumerate(avg_times):
+        print(f"average runtime for {i*step+1} hash-workers: {avg_time:.5e}")
+
+"""
+for thr in THREADS:
+    csv = []
+    # csv = ["{}/{}".format(inp, loop)]
+    for loop in LOOPS:
+        cmd = "./bin/prefix_scan -o temp.txt -n {} -i tests/{} -l {}".format(
+            thr, inp, loop
+        )
+        out = check_output(cmd, shell=True).decode("ascii")
+        m = re.search("time: (.*)", out)
+        if m is not None:
+            time = m.group(1)
+            csv.append(time)
+    csvs.append(csv)
+    sleep(0.5)
+fig, ax = plt.subplots(figsize=(10, 8))
+for idx, csv in enumerate(csvs):
+    csv_int = [int(c) for c in csv]
+    plt.plot(LOOPS, csv_int, label=THREADS[idx])
+plt.xlabel("number of loops")
+plt.ylabel("total runtime")
+ax.grid()
+plt.title("Runtime vs Loops for %s input" % inp)
+
+plt.legend()
+out_png = "graph_part2.png"
+plt.savefig(out_png, dpi=150)
+plt.show()
+plt.close()
+"""
--- src/BST.go
+++ src/BST.go
@@ -31,12 +31,13 @@ func insertNode(root *TreeNode, value int) *TreeNode {
 func main() {
 	var filename string
 	var hash_workers, data_workers, comp_workers int
-	var print_groups bool
+	var print_groups,equal_workers bool
 	flag.StringVar(&filename, "filename", "", "string-valued path to an input file")
 	flag.IntVar(&hash_workers,"hash-workers", 0, "integer-valued number of threads")
 	flag.IntVar(&data_workers,"data-workers", 0, "integer-valued number of threads")
 	flag.IntVar(&comp_workers,"comp-workers", 0, "integer-valued number of threads")
 	flag.BoolVar(&print_groups,"print-groups",true,"print hash groups and compare groups")
+	flag.BoolVar(&equal_workers,"equal-workers",false,"spawn num of goroutines equal to num BSTs")
 	flag.Parse()
 	if filename == "" {
 		fmt.Println("Usage: go run filename.go -filename=sample-file.txt")
@@ -51,7 +52,7 @@ func main() {
 
 	var inputNumbers []int
 	var trees []TreeNode
-	var hashes []int
+	//var hashes []int
 	//idx := 0
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
@@ -78,27 +79,44 @@ func main() {
 		
 		//idx += 1
 	}
+
+	// Calculate hashes
+	hashes := make([]int, len(trees))
+	var num_hashworkers int
+	if hash_workers == 0 {
+		num_hashworkers = 1
+	} else if equal_workers {
+		num_hashworkers = len(trees)
+	} else {
+		num_hashworkers = hash_workers
+	}
 	hash_start := time.Now()
-	for _, bst := range trees {
-		hash_val := 1
-		inOrderTraversal(&bst, &hash_val)
+	calcHashes(&trees,&hashes,num_hashworkers)
+	/*
+	for idx, bst := range trees {
+		hashes[idx] = 1
+		if equal_workers {
+			go inOrderTraversal(&bst,&hashes[idx])
+		} else {
+			inOrderTraversal(&bst, &hashes[idx])
+		}
 		//fmt.Println()
 		//fmt.Println("tree idx: ",idx,"hash_val: ", hash_val)
-		hashes = append(hashes,hash_val)
 	}
+	*/
 	hash_elapsed := time.Since(hash_start)
 	if err := scanner.Err(); err != nil {
 		fmt.Println("Error reading file:", err)
 		return
 	}
-	fmt.Println("hashTime: ",hash_elapsed.Seconds())
+	fmt.Println("hashTime:",hash_elapsed.Seconds())
 	if comp_workers > 0 || data_workers > 0 {
 		//hash_group_start := time.Now()
 		result := findIndices(hashes)
 		//fmt.Println("hash groups:")
 		//hash_idx := 0
 		hash_group_elapsed := time.Since(hash_start)
-		fmt.Println("hashGroupTime: ",hash_group_elapsed.Seconds())
+		fmt.Println("hashGroupTime:",hash_group_elapsed.Seconds())
 		if print_groups {
 			for _, indices := range result {
 				if len(indices) > 1 {
@@ -134,7 +152,7 @@ func main() {
 			}
 		}
 		compare_elapsed := time.Since(compare_start)
-		fmt.Println("compareTreeTime: ",compare_elapsed.Seconds())
+		fmt.Println("compareTreeTime:",compare_elapsed.Seconds())
 		group_idx:=0
 		if print_groups {
 			for _,groups := range compareMap {
@@ -166,6 +184,29 @@ func inOrderTraversal(root *TreeNode, hash_val *int) []int {
 	return res
 }
 
+func calcHashes(tree_list *[]TreeNode, hash_list *[]int, num_workers int) {
+	if num_workers > len(*tree_list) {
+		num_workers = len(*tree_list)
+	}
+	if num_workers <= 1 {
+		for idx := range *tree_list {
+			(*hash_list)[idx] = 1
+			inOrderTraversal(&(*tree_list)[idx], &(*hash_list)[idx])
+		}
+	} else {
+		for i:=0;i<num_workers;i++ {
+			go calcHashesWorker(tree_list,hash_list,num_workers,i)
+		}
+	}
+}
+
+func calcHashesWorker(tree_list *[]TreeNode, hash_list *[]int, num_workers int, idx int) {
+	for idx < len(*tree_list) {
+		inOrderTraversal(&(*tree_list)[idx], &(*hash_list)[idx])
+		idx += num_workers
+	}
+}
+
 func compareTrees(root1 *TreeNode, root2 *TreeNode) bool {
 	var tmp int
 	list1 := inOrderTraversal(root1,&tmp)
@@ -173,8 +214,7 @@ func compareTrees(root1 *TreeNode, root2 *TreeNode) bool {
 	if len(list1) != len(list2) {
 		return false
 	}
-	for i,_ := range list1 {
-		if list1[i] != list2[i] {
+	for i := range list1 {		if list1[i] != list2[i] {
 			return false
 		}
 	}
