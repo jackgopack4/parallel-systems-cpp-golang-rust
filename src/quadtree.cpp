a531b245fd79f05f406c4024ae48634d932f6f28
Blue Lizard <jlpeterson@outlook.com>
2023-12-01 00:08:18 +0000

    add task log

--- src/quadtree.cpp
+++ src/quadtree.cpp
@@ -1,3 +1,10 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <GL/glu.h>
+#include <GL/glut.h>
 #include <iostream>
 #include <vector>
 
@@ -8,7 +15,11 @@ struct Point {
     double x_vel, y_vel;
     double mass;
     Point(int _idx, double _x, double _y) : index(_idx), x(_x), y(_y), x_vel(0.0), y_vel(0.0), mass(-1) {}
-    Point(int _idx, double _x, double _y, double _mass, double _x_vel, double _y_vel) : index(_idx), x(_x), y(_y), x_vel(_x_vel), y_vel(_y_vel), mass(_mass) {}
+    Point(int _idx, double _x, double _y, double _mass, double _x_vel, double _y_vel) : index(_idx), x(_x), y(_y), x_vel(_x_vel), y_vel(_y_vel), mass(_mass) {
+        if (_x >= 4 || _x <= 0 || y >= 4 || y <= 0) {
+            mass = -1.;
+        }
+    }
 };
 void printPoint(const Point& point);
 
@@ -50,6 +61,7 @@ public:
 
     void insert(Node* node, const Point& point) {
       std::cout << "inserting point (" << point.x << ", " << point.y << ")" << std::endl;
+      if (point.x > 0 && point.x < 4 && point.y > 0 && point.y < 4) {
         if (node->isLeaf()) {
             if (node->points.size() < MAX_CAPACITY) {
                 node->points.push_back(point);
@@ -61,6 +73,8 @@ public:
             int index = getIndex(node, point);
             insert(node->children[index], point);
         }
+      }
+
     }
 
     void printTree() const {
@@ -129,18 +143,18 @@ int main() {
     Quadtree quadtree(4, 4);
     quadtree.printTree();
     // Insert some points for testing
-    quadtree.insert(Point(0, 2.054482e+00,	6.003183e-01,	2.647132e+00,	0.000000e+00,	0.000000e+00));
+    quadtree.insert(Point(5,	2.810173e+00,	1.999232e+00,	2.235070e+00,	0.000000e+00,	0.000000e+00));
+    quadtree.insert(Point(0,    2.054482e+00,	6.003183e-01,	2.647132e+00,	0.000000e+00,	0.000000e+00));
     //quadtree.printTree();
     quadtree.insert(Point(1,	2.647132e+00,	3.554528e+00,	2.831508e+00,	0.000000e+00,	0.000000e+00));
     //quadtree.printTree();
+    quadtree.insert(Point(7,	3.985606e+00,	1.332045e-01,	8.786317e-01,	0.000000e+00,	0.000000e+00));
     quadtree.insert(Point(2,	2.831508e+00,	2.876292e+00,	2.996487e+00,	0.000000e+00,	0.000000e+00));
     quadtree.insert(Point(3,	2.996487e+00,	1.272099e+00,	2.288692e+00,	0.000000e+00,	0.000000e+00));
     quadtree.insert(Point(4,	2.288692e+00,	2.443186e+00,	2.810173e+00,	0.000000e+00,	0.000000e+00));
-    quadtree.insert(Point(5,	2.810173e+00,	1.999232e+00,	2.235070e+00,	0.000000e+00,	0.000000e+00));
-    quadtree.insert(Point(6,	2.235070e+00,	2.731189e+00,	3.985606e+00,	0.000000e+00,	0.000000e+00));
-    quadtree.insert(Point(7,	3.985606e+00,	1.332045e-01,	8.786317e-01,	0.000000e+00,	0.000000e+00));
-    quadtree.insert(Point(8,	8.786317e-01,	3.940775e+00,	8.985912e-01,	0.000000e+00,	0.000000e+00));
     quadtree.insert(Point(9,	8.985912e-01,	1.985868e+00,	1.896852e+00,	0.000000e+00,	0.000000e+00));
+    quadtree.insert(Point(6,	2.235070e+00,	2.731189e+00,	3.985606e+00,	0.000000e+00,	0.000000e+00));
+    quadtree.insert(Point(8,	8.786317e-01,	3.940775e+00,	8.985912e-01,	0.000000e+00,	0.000000e+00));
     quadtree.printTree();
     return 0;
 }
